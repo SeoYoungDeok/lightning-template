af9c3797e11665785fb43be665173050ee559a90
SeoYoungDeok <dud1933@naver.com>
2023-08-21 20:19:36 +0900

    Feat : .env 추가
    - .env 추가
    - .env.example 추가

--- src/config/logger.py
+++ src/config/logger.py
@@ -1,10 +1,18 @@
+import os
+
+from dotenv import load_dotenv
 from hydra.core.config_store import ConfigStore
 from lightning.pytorch.loggers.wandb import WandbLogger
 
 from src.config import full_builds
 
+load_dotenv()
+
 WandbLoggerConfig = full_builds(
-    WandbLogger, entity="whitegreen", project="test", name="lightning-test"
+    WandbLogger,
+    entity=os.environ["WANDB_ENTITY"],
+    project=os.environ["WANDB_PROJECT"],
+    name=os.environ["WANDB_RUNNAME"],
 )
 
 
